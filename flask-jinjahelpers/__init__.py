aee359eeb0c2c0b63f53d2bd775c36cf9824ce83
Konsta Vesterinen <konsta.vesterinen@gmail.com>
2012-11-14 10:43:24 +0200

    initial draft

---
+++ flask-jinjahelpers/__init__.py
@@ -0,0 +1,76 @@
+from flask import request, url_for
+from werkzeug.datastructures import MultiDict
+
+
+def qp_url_for(endpoint, **kwargs):
+    """
+    Returns the url for an endpoint but preserves all request query parameters
+
+    kwargs can be used for overriding specific query parameters
+
+    :param endpoint: endpoint to return the url for, eg. user.index
+    :param kwargs: dict containing query parameter names as keys
+    """
+    data = dict(MultiDict(request.args).lists())
+
+    for key, value in kwargs.items():
+        data[key] = value
+    return url_for(endpoint, **data)
+
+
+def visible_page_numbers(page, pages, inner_window=3, outer_window=0):
+    """
+    Takes the current page number and total number of pages, and computes
+    an array containing the visible page numbers.  At least three pages on
+    either side of the current page as well as the first and last pages
+    will be included. For example::
+
+        [1] 2 3 4 5 6 7 ... 42
+        1 2 3 4 5 [6] 7 ... 42
+        1 ... 4 5 6 [7] 8 9 ... 42
+        1 ... 36 37 38 39 [40] 41 42
+
+    :param page: current page number
+    :param pages: total number of pages
+    """
+    window_from = page - inner_window
+    window_to = page + inner_window
+
+    if window_to > pages:
+        window_from -= window_to - pages
+        window_to = pages
+
+    if window_from < 1:
+        window_to += 1 - window_from
+        window_from = 1
+        if window_to > pages:
+            window_to = pages
+
+    visible = []
+
+    left_gap_start = min(2 + outer_window, window_from)
+    left_gap_end = window_from - 1
+
+    for page in xrange(1, left_gap_start):
+        visible.append(page)
+
+    if left_gap_end - left_gap_start > 0:
+        visible.append('...')
+    elif left_gap_start == left_gap_end:
+        visible.append(left_gap_start)
+
+    right_gap_start = min(window_to + 1, pages - outer_window)
+    right_gap_end = pages - outer_window - 1
+
+    for page in xrange(left_gap_end + 1, right_gap_start):
+        visible.append(page)
+
+    if right_gap_end - right_gap_start > 0:
+        visible.append('...')
+    elif right_gap_start == right_gap_end:
+        visible.append(right_gap_start)
+
+    for page in xrange(right_gap_end + 1, pages + 1):
+        visible.append(page)
+
+    return visible
---
+++ setup.py
@@ -0,0 +1,54 @@
+"""
+Flask-Alchemy
+---------------
+
+Various helpers for Flask based Jinja2 templates.
+"""
+
+from setuptools import setup, Command
+import subprocess
+
+
+class PyTest(Command):
+    user_options = []
+
+    def initialize_options(self):
+        pass
+
+    def finalize_options(self):
+        pass
+
+    def run(self):
+        errno = subprocess.call(['py.test'])
+        raise SystemExit(errno)
+
+
+setup(
+    name='flask-jinjahelpers',
+    version='0.1.0',
+    url='https://github.com/kvesteri/flask-jinjahelpers',
+    license='BSD',
+    author='Konsta Vesterinen',
+    author_email='konsta@fastmonkeys.com',
+    description=(
+        'Various helpers for Flask based Jinja2 templates.'
+    ),
+    long_description=__doc__,
+    packages=['flask_jinjahelpers'],
+    zip_safe=False,
+    include_package_data=True,
+    platforms='any',
+    install_requires=[
+        'Flask>=0.8',
+    ],
+    cmdclass={'test': PyTest},
+    classifiers=[
+        'Environment :: Web Environment',
+        'Intended Audience :: Developers',
+        'License :: OSI Approved :: BSD License',
+        'Operating System :: OS Independent',
+        'Programming Language :: Python',
+        'Topic :: Internet :: WWW/HTTP :: Dynamic Content',
+        'Topic :: Software Development :: Libraries :: Python Modules'
+    ]
+)
