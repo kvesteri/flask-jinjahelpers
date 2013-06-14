d67d6be1986f757fa2e080479265c236c80a01c6
Konsta Vesterinen <konsta.vesterinen@gmail.com>
2013-06-14 14:27:45 +0300

    Bumped version

--- setup.py
+++ setup.py
@@ -25,7 +25,7 @@ class PyTest(Command):
 
 setup(
     name='Flask-JinjaHelpers',
-    version='0.2.4',
+    version='0.3.0',
     url='https://github.com/kvesteri/flask-jinjahelpers',
     license='BSD',
     author='Konsta Vesterinen',
